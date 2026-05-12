6e1a0fde49c28b5d16cac49a6de9d76da437204b
Smriti Singh <smritisingh9118@gmail.com>
2026-05-13 04:02:48 +0530

    Removed unwanted files

--- project2/index.js
+++
@@ -1,7 +0,0 @@
-const parent=document.getElementById('parent');
-parent.addEventListener('click',(e)=>{
-    console.log(e.target);
-    const child=e.target;
-    const body = document.querySelector('body')
-    body.style.backgroundColor=child.id;
-})
--- project2/p5.js
+++
@@ -1,8 +0,0 @@
-const div = document.querySelector('div');
-setInterval(()=>{
-    let time = new Date();
-    div.textContent = time.toLocaleTimeString();
-    
-},1000)
-
-
